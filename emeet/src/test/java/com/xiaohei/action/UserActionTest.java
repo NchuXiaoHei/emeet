03af572e3274a9ed760b9d35b11df14ae1bb8145
ChengLu <ChengLu@ChengLu-PC>
2018-05-22 17:12:31 +0800

    添加主页。调整静态资源的路径（css，js）

--- emeet/src/test/java/com/xiaohei/action/UserActionTest.java
+++ emeet/src/test/java/com/xiaohei/action/UserActionTest.java
@@ -22,6 +22,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.xiaohei.controller.UserController;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration(value = "src/main/webapp")
@@ -37,7 +39,7 @@ public class UserActionTest {
 	
 	
 	@Autowired
-	UserAction userAction;
+	UserController userAction;
 	@Autowired
 	ServletContext context;
 
